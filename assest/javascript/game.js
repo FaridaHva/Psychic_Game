49e037ac8af473d25ee30030ecefff08ea405af3
FaridaHva <faridahva20@gmail.com>
2022-09-08 18:56:28 +0400

    Completed

--- assest/javascript/game.js
+++ assest/javascript/game.js
@@ -1,4 +1,3 @@
-// An array of values for the computer to randomly choose from. 
 var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]                
 
